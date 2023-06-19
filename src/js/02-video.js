92c4cd05d98e941755aec9ca95b1203f9669c34b
TarasDemchenko <tarasdemchenko1993@gmail.com>
2023-06-19 17:00:37 +0200

    home 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Vimeo(document.getElementById('vimeo-player'));
+
+const savePlaybackTime = throttle(function (currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime.toString());
+}, 1000);
+
+player.on('timeupdate', function (data) {
+  const currentTime = data.seconds;
+  savePlaybackTime(currentTime);
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime !== null) {
+  player.ready().then(function () {
+    player.setCurrentTime(parseFloat(savedTime));
+  });
+} else {
+  player.ready().then(function () {
+    player.play();
+  });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,58 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+let saveFormStateTimeout;
 
+const saveFormState = () => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+};
+
+const throttle = (callback, delay) => {
+  let previousCall = new Date().getTime();
+  return function () {
+    const time = new Date().getTime();
+    if (time - previousCall >= delay) {
+      previousCall = time;
+      callback.apply(null, arguments);
+    }
+  };
+};
+
+const throttledSaveFormState = throttle(saveFormState, 1000);
+
+const loadFormState = () => {
+  const savedState = localStorage.getItem('feedback-form-state');
+  if (savedState) {
+    const formState = JSON.parse(savedState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+};
+
+const clearFormState = () => {
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+form.addEventListener('input', () => {
+  clearTimeout(saveFormStateTimeout);
+  saveFormStateTimeout = setTimeout(throttledSaveFormState, 1000);
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log({
+    email: emailInput.value,
+    message: messageInput.value,
+  });
+  clearFormState();
+});
+
+window.addEventListener('DOMContentLoaded', loadFormState);
+
+window.addEventListener('unload', saveFormState);
